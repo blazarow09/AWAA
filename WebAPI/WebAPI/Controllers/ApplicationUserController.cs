b21610a573369f0c229f6ef0ae47d91a02995fa0
blazarow09 <blazarow09@gmail.com>
2019-05-18 01:58:24 +0300

    Upload WebApi with implemented register functionality

---
+++ WebAPI/WebAPI/Controllers/ApplicationUserController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationUserController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public ApplicationUserController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register(ApplicationUserModel model)
+        {
+            var applicationUser = new ApplicationUser()
+            {
+                UserName = model.Username,
+                Email = model.Email,
+                FullName = model.FullName
+            };
+
+            try
+            {
+                var result = await this.userManager.CreateAsync(applicationUser, model.Password);
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
