52173b85440d8fccb4569969bc7e802a742a69f8
blazarow09 <blazarow09@gmail.com>
2019-05-18 17:33:57 +0300

    upload user profile controller

---
+++ WebAPI/WebAPI/Controllers/UserProfileController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserProfileController : ControllerBase
+    {
+        private UserManager<ApplicationUser> userManager;
+
+        public UserProfileController(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<Object> GetUserProfile()
+        {
+            string userId = User.Claims.First(x => x.Type == "UserID").Value;
+            var user = await this.userManager.FindByIdAsync(userId);
+
+            return new
+            {
+                user.FullName,
+                user.Email,
+                user.UserName
+            };
+        }
+    }
+}
