704d53a7f5a6a36b167928d73d8541c99404c1e1
blazarow09 <blazarow09@gmail.com>
2019-05-18 17:34:46 +0300

    upload app user login model

---
+++ WebAPI/WebAPI/Models/ApplicationUserLoginModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPI.Models
+{
+    public class ApplicationUserLoginModel
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+    }
+}
